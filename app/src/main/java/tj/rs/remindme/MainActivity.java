a554c2de769e3b0bb60cde7f2f7e764ca0e65921
rustamsafarovrs <rs.programmer@hotmail.com>
2019-01-22 13:58:01 +0500

    Start to implementing NavigationView.

--- app/src/main/java/tj/rs/remindme/MainActivity.java
+++ app/src/main/java/tj/rs/remindme/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppThemeDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
